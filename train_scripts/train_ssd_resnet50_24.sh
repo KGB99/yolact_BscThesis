be6b6ee9f13ffafacf078a06d83c758a39f7e815
KGB99 <kbirgi@gmail.com>
2023-11-18 13:00:44 +0100

    added new train script

--- train_scripts/train_ssd_resnet50_24.sh
+++ train_scripts/train_ssd_resnet50_24.sh
@@ -3,8 +3,8 @@
 #SBATCH -n 1
 #SBATCH --gpus=1
 #SBATCH --mem-per-cpu=20G
-#SBATCH --time=4:00:00
+#SBATCH --time=12:00:00
 
 module load gcc/8.2.0 python_gpu/3.11.2
-python3 train.py --batch_size=8 --num_workers=1 --config=medical_00100x_resnet50_config
+python3 train.py --batch_size=8 --num_workers=1 --config=medical_ssd_resnet50_config
 # batch_size=8*(num_GPUS)
