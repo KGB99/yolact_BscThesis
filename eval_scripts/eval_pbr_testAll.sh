071b11ea9ebda2e7256d7ea396204a82424709bc
KGB99 <kbirgi@gmail.com>
2024-01-20 12:37:38 +0100

    update script arguments

---
+++ eval_scripts/eval_pbr_testAll.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=80G
+#SBATCH --time=4:00:00
+
+module load gcc/8.2.0 python_gpu/3.11.2
+python3 eval.py --trained_model=weights/pbr_amodal_resnet50_3_20000.pth --config=pbr_amodal_resnet50_20000 --output_coco_json --bbox_det_file=results/pbr_testSSD_bbox_detections.json --mask_det_file=results/pbr_testSSD_mask_detections.json --dataset=test_all --score_threshold=0.2 --top_k=5
