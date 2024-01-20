0d08305bc89f02a11e55b4ac72f37ab99b7f7d64
KGB99 <kbirgi@gmail.com>
2024-01-20 12:38:03 +0100

    script for using pbr model instead of real

--- sampling_pbr.sh
+++ sampling_pbr.sh
@@ -4,5 +4,5 @@
 #SBATCH --mem-per-cpu=40G
 #SBATCH --time=4:00:00
 module load gcc/8.2.0 python_gpu/3.11.2
-python3 sampling_experiments.py --images_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp --trained_model=weights/pbr_modal_resnet50_old_3_20000.pth --coco_file=/cluster/project/infk/cvg/heinj/students/kbirgi/Annotations/testAll/subset_amodal_labels.json
+python3 sampling_experiments.py --images_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp --trained_model=weights/pbr_amodal_resnet50_3_20000.pth --coco_file=/cluster/project/infk/cvg/heinj/students/kbirgi/Annotations/testAll/subset_amodal_labels.json
 
