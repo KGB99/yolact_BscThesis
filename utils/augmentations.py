345c25c278a662e5c68f02ea6f429ccd7140c6d4
KGB99 <k.birgi@gmail.com>
2024-02-07 09:52:49 +0100

    update visualizer and print for augmentation image

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -522,6 +522,8 @@ class PhotometricDistort(object):
         else:
             distort = Compose(self.pd[1:])
         im, masks, boxes, labels = distort(im, masks, boxes, labels)
+        cv2.imwrite("./testerOutput/image_augmentation.png", im)
+        exit()
         return self.rand_light_noise(im, masks, boxes, labels)
 
 class PrepareMasks(object):
