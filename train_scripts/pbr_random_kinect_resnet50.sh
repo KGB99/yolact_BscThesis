d8b04fa6d8a6e2babfd5d295405b8f04ceabd0eb
KGB99 <k.birgi@gmail.com>
2024-02-10 17:37:34 +0100

    update scripts

--- train_scripts/pbr_random_kinect_resnet50.sh
+++ train_scripts/pbr_random_kinect_resnet50.sh
@@ -5,5 +5,7 @@
 #SBATCH --mem-per-cpu=80G
 #SBATCH --time=24:00:00
 
-module load gcc/8.2.0 python_gpu/3.11.2
-python3 train.py --batch_size=8 --num_workers=1 --config=train_pbr_random_and_kinect_20000
+module load gcc/8.2.0 python_gpu/3.11.2 eth_proxy
+source myenv/bin/activate
+python3 train.py --batch_size=8 --num_workers=1 --config=train_pbr_random_and_kinect_40000
+deactivate
--- train_scripts/quick_pbr_random_kinect_resnet50.sh
+++ train_scripts/quick_pbr_random_kinect_resnet50.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem-per-cpu=80G
 #SBATCH --time=24:00:00
 
-#module load gcc/8.2.0 python_gpu/3.11.2
+module load gcc/8.2.0 python_gpu/3.11.2
 source myenv/bin/activate
 python3 train.py --batch_size=8 --num_workers=1 --config=train_pbr_random_and_kinect_1500
 deactivate
