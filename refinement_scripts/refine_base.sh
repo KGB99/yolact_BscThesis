60f7eb2fcb09b118a768614ba6c3f93275a77429
KGB99 <k.birgi@gmail.com>
2024-02-15 19:51:26 +0100

    add first refinement script

---
+++ refinement_scripts/refine_base.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=80G
+#SBATCH --time=24:00:00
+
+module load gcc/8.2.0 python_gpu/3.11.2 eth_proxy
+source myenv/bin/activate
+python3 train.py --batch_size=8 --num_workers=1 --resume=pbr_random_and_kinect_base_3_20000.pth -- --start_iter=0 --config=refinement_pbr_kinect_random_real_1000
+deactivate
