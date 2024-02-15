5086484d4eddbed9795a966ba3c107948bf80e09
KGB99 <k.birgi@gmail.com>
2024-02-15 19:51:00 +0100

    add refinement_training

--- train.py
+++ train.py
@@ -80,6 +80,7 @@ parser.add_argument('--batch_alloc', default=None, type=str,
 parser.add_argument('--no_autoscale', dest='autoscale', action='store_false',
                     help='YOLACT will automatically scale the lr and the number of iterations depending on the batch size. Set this if you want to disable that.')
 parser.add_argument('--refinement_mode', default=False, required=False, type=bool, help='for the refinement training of real data on a pretrained pbr model')
+parser.add_argument('--refinement_iterations', default=1000, required=False, type=int, help='iterations for refinement')
 
 parser.set_defaults(keep_latest=False, log=True, log_gpu=False, interrupt=True, autoscale=True)
 args = parser.parse_args()
@@ -170,6 +171,296 @@ class CustomDataParallel(nn.DataParallel):
             out[k] = torch.stack([output[k].to(output_device) for output in outputs])
         
         return out
+    
+def refinement_training():
+    if not os.path.exists(args.save_folder):
+        os.mkdir(args.save_folder)
+
+    dataset = COCODetection(image_path=cfg.dataset.train_images,
+                            info_file=cfg.dataset.train_info,
+                            transform=SSDAugmentation(MEANS))
+    
+    refinement_dataset = COCODetection(image_path = cfg.dataset.refinement_images,
+                                        info_fil=cfg.dataset.refinement_info,
+                                        transform=SSDAugmentation(MEANS))
+    
+    if args.validation_epoch > 0:
+        setup_eval()
+        val_dataset = COCODetection(image_path=cfg.dataset.valid_images,
+                                    info_file=cfg.dataset.valid_info,
+                                    transform=BaseTransform(MEANS))
+
+    # Parallel wraps the underlying module, but when saving and loading we don't want that
+    yolact_net = Yolact()
+    net = yolact_net
+    net.train()
+
+    if args.log:
+        log = Log(cfg.name, args.log_folder, dict(args._get_kwargs()),
+            overwrite=(args.resume is None), log_gpu_stats=args.log_gpu)
+
+    # I don't use the timer during training (I use a different timing method).
+    # Apparently there's a race condition with multiple GPUs, so disable it just to be safe.
+    timer.disable_all()
+
+    # Both of these can set args.resume to None, so do them before the check    
+    if args.resume == 'interrupt':
+        args.resume = SavePath.get_interrupt(args.save_folder)
+    elif args.resume == 'latest':
+        args.resume = SavePath.get_latest(args.save_folder, cfg.name)
+
+    if args.resume is not None:
+        print('Resuming training, loading {}...'.format(args.resume))
+        yolact_net.load_weights(args.resume)
+
+        if args.start_iter == -1:
+            args.start_iter = SavePath.from_str(args.resume).iteration
+    else:
+        print('Initializing weights...')
+        yolact_net.init_weights(backbone_path=args.save_folder + cfg.backbone.path)
+
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum,
+                          weight_decay=args.decay)
+    criterion = MultiBoxLoss(num_classes=cfg.num_classes,
+                             pos_threshold=cfg.positive_iou_threshold,
+                             neg_threshold=cfg.negative_iou_threshold,
+                             negpos_ratio=cfg.ohem_negpos_ratio)
+
+    if args.batch_alloc is not None:
+        args.batch_alloc = [int(x) for x in args.batch_alloc.split(',')]
+        if sum(args.batch_alloc) != args.batch_size:
+            print('Error: Batch allocation (%s) does not sum to batch size (%s).' % (args.batch_alloc, args.batch_size))
+            exit(-1)
+
+    net = CustomDataParallel(NetLoss(net, criterion))
+    if args.cuda:
+        net = net.cuda()
+    
+    # Initialize everything
+    if not cfg.freeze_bn: yolact_net.freeze_bn() # Freeze bn so we don't kill our means
+    yolact_net(torch.zeros(1, 3, cfg.max_size, cfg.max_size).cuda())
+    if not cfg.freeze_bn: yolact_net.freeze_bn(True)
+
+    # loss counters
+    loc_loss = 0
+    conf_loss = 0
+    iteration = max(args.start_iter, 0)
+    last_time = time.time()
+    print("Number of iterations: " + str(iteration))
+    epoch_size = len(dataset) // args.batch_size
+    num_epochs = math.ceil(cfg.max_iter / epoch_size)
+    #print("size per epoch: " + str(epoch_size))
+    #print("number of epochs: " + str(num_epochs))
+
+    # Which learning rate adjustment step are we on? lr' = lr * gamma ^ step_index
+    step_index = 0
+
+    pbr_data_loader = data.DataLoader(dataset, args.batch_size,
+                                  num_workers=args.num_workers,
+                                  shuffle=True, collate_fn=detection_collate,
+                                  pin_memory=True,
+                                  generator=torch.Generator(device='cuda'))
+    pbr_iterator = iter(pbr_data_loader)
+
+    
+    real_data_loader = data.DataLoader(refinement_dataset, args.batch_size,
+                                        num_workers=args.num_workers,
+                                        shuffle=True, collate_fn=detection_collate,
+                                        pin_memory=True,
+                                        generator=torch.Generator(device='cuda'))
+    real_iterator = iter(real_data_loader)
+    
+    # need a second data loader for the validation set
+    # note that the val_dataset uses BaseTransform as transformation, not SSDAugmentation like during training
+    # this is because during training the SSDAugmentation also randomly flips,etc... images for robustness
+    val_data_loader = data.DataLoader(val_dataset, args.batch_size,
+                                      num_workers=args.num_workers,
+                                      shuffle=False, collate_fn=detection_collate,
+                                      pin_memory=True,
+                                      generator=torch.Generator(device='cuda'))
+    
+    save_path = lambda epoch, iteration: SavePath(cfg.name, epoch, iteration).get_path(root=args.save_folder)
+    time_avg = MovingAverage()
+
+    global loss_types # Forms the print order
+    loss_avgs  = { k: MovingAverage(100) for k in loss_types }
+
+    #create a dict for the val_loss
+    #val_loss_avgs = { k: MovingAverage(100) for k in loss_types}
+
+    print('Begin training!')
+    print()
+    # try-except so you can use ctrl+c to save early and stop training
+    try:
+        epoch=0
+        while(True):
+
+            # Resume from start_iter
+            #if (epoch+1)*epoch_size < iteration:
+            #    continue
+
+            #print("DEBUGGING VAL LOSS")
+            #compute_validation_loss(net, val_data_loader, log, epoch)
+            
+            #for datum in data_loader:
+            if np.random.rand() < cfg.ratio_pbr_to_real:
+                try:
+                    datum = next(pbr_iterator)
+                except StopIteration: # incase the dataloader is exhausted
+                    pbr_iterator = iter(pbr_data_loader)
+                    datum = next(pbr_iterator)
+            else:
+                try:
+                    datum = next(real_iterator)
+                except StopIteration:
+                    real_iterator = iter(real_data_loader)
+                    datum = next(real_data_loader)
+            
+
+            
+            # Stop if we've reached an epoch if we're resuming from start_iter
+            #if iteration == (epoch+1)*epoch_size:
+            #    break
+
+            # Stop at the configured number of iterations even if mid-epoch
+            if iteration == cfg.max_iter:
+                break
+            
+            #for augmentation debug
+            #exit() 
+            
+            # Change a config setting if we've reached the specified iteration
+            changed = False
+            for change in cfg.delayed_settings:
+                if iteration >= change[0]:
+                    changed = True
+                    cfg.replace(change[1])
+
+                    # Reset the loss averages because things might have changed
+                    for avg in loss_avgs:
+                        avg.reset()
+            
+            # If a config setting was changed, remove it from the list so we don't keep checking
+            if changed:
+                cfg.delayed_settings = [x for x in cfg.delayed_settings if x[0] > iteration]
+
+            # Warm up by linearly interpolating the learning rate from some smaller value
+            if cfg.lr_warmup_until > 0 and iteration <= cfg.lr_warmup_until:
+                set_lr(optimizer, (args.lr - cfg.lr_warmup_init) * (iteration / cfg.lr_warmup_until) + cfg.lr_warmup_init)
+
+            # Adjust the learning rate at the given iterations, but also if we resume from past that iteration
+            while step_index < len(cfg.lr_steps) and iteration >= cfg.lr_steps[step_index]:
+                step_index += 1
+                set_lr(optimizer, args.lr * (args.gamma ** step_index))
+            
+            # Zero the grad to get ready to compute gradients
+            optimizer.zero_grad()
+
+            # Forward Pass + Compute loss at the same time (see CustomDataParallel and NetLoss)
+            losses = net(datum)
+            
+            losses = { k: (v).mean() for k,v in losses.items() } # Mean here because Dataparallel
+            loss = sum([losses[k] for k in losses])
+            
+            #avg loss for wandb?
+            #avg_loss += loss.item()
+            
+
+            # no_inf_mean removes some components from the loss, so make sure to backward through all of it
+            # all_loss = sum([v.mean() for v in losses.values()])
+
+            # Backprop
+            loss.backward() # Do this to free up vram even if loss is not finite
+            if torch.isfinite(loss).item():
+                optimizer.step()
+            
+            # Add the loss to the moving average for bookkeeping
+            for k in losses:
+                loss_avgs[k].add(losses[k].item())
+
+            cur_time  = time.time()
+            elapsed   = cur_time - last_time
+            last_time = cur_time
+
+            # Exclude graph setup from the timing information
+            if iteration != args.start_iter:
+                time_avg.add(elapsed)
+
+            if iteration % 10 == 0:
+                eta_str = str(datetime.timedelta(seconds=(cfg.max_iter-iteration) * time_avg.get_avg())).split('.')[0]
+                
+                total = sum([loss_avgs[k].get_avg() for k in losses])
+                loss_labels = sum([[k, loss_avgs[k].get_avg()] for k in loss_types if k in losses], [])
+                if WANDB:
+                    loss_dict = {}
+                    for k in losses:
+                        loss_dict['moving_avg_loss_' + k] = round(loss_avgs[k].get_avg(),5)
+                    wandb.log(loss_dict, step=iteration)
+                
+                print(('Epoch: %3d | Iteration %7d ||' + (' %s: %.3f |' * len(losses)) + ' T: %.3f || ETA: %s || timer: %.3f')
+                        % tuple([iteration] + loss_labels + [total, eta_str, elapsed]), flush=True)
+                
+
+            if args.log:
+                precision = 5
+                loss_info = {k: round(losses[k].item(), precision) for k in losses}
+                loss_info['T'] = round(loss.item(), precision)
+                if WANDB:
+                    loss_dict = {}
+                    loss_dict = {'loss_'+k: round(losses[k].item(),precision) for k in losses}
+                    loss_dict['loss_T'] = round(loss.item(), precision)
+                    wandb.log(loss_dict, step=iteration)
+                
+                
+                if args.log_gpu:
+                    log.log_gpu_stats = (iteration % 10 == 0) # nvidia-smi is sloooow
+                    
+                log.log('train', loss=loss_info, epoch=epoch, iter=iteration,
+                    lr=round(cur_lr, 10), elapsed=elapsed)
+
+                log.log_gpu_stats = args.log_gpu
+            
+            #also compute validation loss every 3000 iterations
+            if iteration > 0 and iteration % 3000 == 0:
+                compute_validation_loss(net, val_data_loader, log, epoch, iteration)
+
+            iteration += 1
+
+            if iteration % args.save_interval == 0 and iteration != args.start_iter:
+                if args.keep_latest:
+                    latest = SavePath.get_latest(args.save_folder, cfg.name)
+
+                print('Saving state, iter:', iteration)
+                yolact_net.save_weights(save_path(epoch, iteration))
+
+                if args.keep_latest and latest is not None:
+                    if args.keep_latest_interval <= 0 or iteration % args.keep_latest_interval != args.save_interval:
+                        print('Deleting old save...')
+                        os.remove(latest)
+
+
+            
+            # This is done per epoch
+            if args.validation_epoch > 0:
+                if epoch % args.validation_epoch == 0 and epoch > 0:
+                    compute_validation_map(epoch, iteration, yolact_net, val_dataset, log if args.log else None)
+        
+        print("Training is done! Now computing validation mAP a final time...")
+        
+        # Compute validation mAP after training is finished
+        compute_validation_map(epoch, iteration, yolact_net, val_dataset, log if args.log else None)
+    except KeyboardInterrupt:
+        if args.interrupt:
+            print('Stopping early. Saving network...')
+            
+            # Delete previous copy of the interrupted network so we don't spam the weights folder
+            SavePath.remove_interrupt(args.save_folder)
+            
+            yolact_net.save_weights(save_path(epoch, repr(iteration) + '_interrupt'))
+        exit()
+
+    yolact_net.save_weights(save_path(epoch, iteration))
+    return
 
 def train():
     if not os.path.exists(args.save_folder):
@@ -288,6 +579,8 @@ def train():
 
     print('Begin training!')
     print()
+    if args.refinement_mode:
+        refinement_training()
     # try-except so you can use ctrl+c to save early and stop training
     try:
         for epoch in range(num_epochs):
@@ -671,7 +964,10 @@ if __name__ == '__main__':
                     'iterations' : cfg.max_iter, 
                 }
             )
-        train()
+        if args.refinement_node:
+            refinement_training()
+        else:
+            train()
     finally:    
         #finish wandb, unsure if this is actually necessary
         if WANDB:
