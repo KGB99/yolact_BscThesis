d4cbd083c998897fc9327c1507d6ddcfd8ce1f36
KGB99 <k.birgi@gmail.com>
2024-02-21 14:01:29 +0100

    update scripts

---
+++ refinement_scripts/refine_lre4_check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=80G
+#SBATCH --time=24:00:00
+
+module load gcc/8.2.0 python_gpu/3.11.2 eth_proxy
+source myenv/bin/activate
+python3 train.py --batch_size=8 --num_workers=1 --resume=weights/pbr_random_and_kinect_aug_hue_5_30000.pth --start_epoch=5 --start_iter=30000 --config=refinement_pbr_aug_hue_all_only_real_lre4_check --refinement_mode=True --save_interval=250
+deactivate
