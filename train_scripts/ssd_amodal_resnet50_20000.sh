4972be6db925babb42b60b8a2ed109f436d016f3
KGB99 <k.birgi@gmail.com>
2024-02-27 19:46:55 +0100

    script updates

--- train_scripts/ssd_amodal_resnet50_20000.sh
+++ train_scripts/ssd_amodal_resnet50_20000.sh
@@ -6,5 +6,6 @@
 #SBATCH --time=24:00:00
 
 module load gcc/8.2.0 python_gpu/3.11.2
+source myenv/bin/activate
 python3 train.py --save_interval=2500 --batch_size=8 --num_workers=1 --config=ssd_amodal_resnet50_20000_config
-# batch_size=8*(num_GPUS)
+deactivate
