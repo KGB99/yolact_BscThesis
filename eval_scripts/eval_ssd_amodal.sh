c6694792d09af2a6cf1a11d17132c7f654d7a500
KGB99 <k.birgi@gmail.com>
2024-03-02 14:36:11 +0100

    update ssd script for eval

--- eval_scripts/eval_ssd_amodal.sh
+++ eval_scripts/eval_ssd_amodal.sh
@@ -7,5 +7,5 @@
 
 module load gcc/8.2.0 python_gpu/3.11.2
 source myenv/bin/activate
-python3 eval.py --trained_model=weights/medical_resnet50_0_5000.pth  --config=medical_00100x_resnet50_config --output_coco_json --bbox_det_file=results/ssd_amodal_bbox_detections.json --mask_det_file=results/ssd_amodal_mask_detections.json --dataset=trial_quant_eval --score_threshold=0.2 --top_k=5
+python3 eval.py --trained_model=weights/ssd_amodal_resnet50_1_20000.pth  --config=ssd_amodal_resnet50_20000_config --output_coco_json --bbox_det_file=results/ssd_amodal_bbox_detections.json --mask_det_file=results/ssd_amodal_mask_detections.json --dataset=trial_quant_eval --score_threshold=0.0 --top_k=5
 deactivate
