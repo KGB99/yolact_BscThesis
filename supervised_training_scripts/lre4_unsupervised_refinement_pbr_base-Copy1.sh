327ff1b8fb959ec720038233cca089187a8d92cc
KGB99 <k.birgi@gmail.com>
2024-04-10 11:27:46 +0200

    final scripts pre cleanup

---
+++ supervised_training_scripts/lre4_unsupervised_refinement_pbr_base-Copy1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=80G
+#SBATCH --time=24:00:00
+
+module load gcc/8.2.0 python_gpu/3.11.2 eth_proxy
+source myenv/bin/activate
+python3 train.py --batch_size=8 --num_workers=1 --config=ref_self_supervised_pbr_base_lre4 --save_interval=1000 --resume=weights/pbr_random_and_kinect_5_30000.pth --start_iter=30000 --refinement_mode=True
+deactivate
---
+++ supervised_training_scripts/lre4_unsupervised_refinement_pbr_real-Copy1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=80G
+#SBATCH --time=24:00:00
+
+module load gcc/8.2.0 python_gpu/3.11.2 eth_proxy
+source myenv/bin/activate
+python3 train.py --batch_size=8 --num_workers=1 --config=ref_self_supervised_pbr_ref_lre4 --save_interval=1000 --resume=weights/refinement_pbr_aug_hue_all_31_24000.pth --start_iter=24000 --refinement_mode=True
+deactivate
